177cbe422819433571db8c59687a458528e9892c
Noham Coulon <44196664+coucoul38@users.noreply.github.com>
2024-03-04 19:47:16 +0100

    added my timer classe :smiley:

---
+++ Project1/Timer.h
@@ -0,0 +1,26 @@
+#pragma once
+class Timer
+{
+public:
+	Timer();
+	float Get();
+	float GetDT();
+	void Reset();
+	void Stop();
+	void Resume();
+	void SetSpeed(float speed);
+	void Tick();
+	~Timer();
+private:
+	float fSystemTime;
+	float fPreviousSystemTime = 0.0f;
+	float fGameTime;
+	float fDeltaTime;
+
+	const float maxFps = 20;
+	const float fMinDelta = 1000/maxFps; // minimum frame time in s
+	const float fMaxDelta = 50; // maximum time in s
+
+	float fSpeed; // time multiplier
+	float fPreviousSpeed;
+};
