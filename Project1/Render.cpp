5f480373cd07d52b9434dff4e9d6ffdf6f616104
Urukagall <djorgeafonso@gaming.tech>
2024-03-07 16:00:53 +0100

    s

--- Project1/Render.cpp
+++ Project1/Render.cpp
@@ -326,7 +326,6 @@ void Render::OnMouseMove(WPARAM btnState, int x, int y)
 
 void Render::BuildDescriptorHeaps()
 {
-	
 	D3D12_DESCRIPTOR_HEAP_DESC cbvHeapDesc;
 	cbvHeapDesc.NumDescriptors = 1;
 	cbvHeapDesc.Type = D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV;
