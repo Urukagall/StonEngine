f4f759dc6c85ff09eb72bc5ac3df8d6f846c05e4
Noham Coulon <44196664+coucoul38@users.noreply.github.com>
2024-03-13 18:59:10 +0100

    lol

--- Game/Missile.cpp
+++ Game/Missile.cpp
@@ -6,9 +6,7 @@ Missile::Missile(Entity* pEntity) : Script(pEntity) {
 
 void Missile::OnLoad()
 {
-	// créer une droite qui part devant le joueur
-	// check distance des ennemis à la droite
-	// lock l'ennemi le plus proche
+	
 }
 
 void Missile::Update(float dt) {
